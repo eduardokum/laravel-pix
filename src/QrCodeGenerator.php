959f2b57abb9f8fa33b9e9e5b41870c802bf4a66
Eduardo Gusmão <eduguscontra3@hotmail.com>
2024-01-18 10:07:38 +0000

    versao qrcode

--- src/QrCodeGenerator.php
+++ src/QrCodeGenerator.php
@@ -18,22 +18,20 @@ class QrCodeGenerator implements GeneratesQrCode
     const FORMAT_JPG = 'jpg';
     const FORMAT_PNG = 'png';
     const FORMAT_WEBP = 'webp';
-    const FORMAT_IMAGICK = 'imagick';
 
     public function __construct($format = 'svg')
     {
-        if (! in_array($format, [self::FORMAT_SVG, self::FORMAT_BMP, self::FORMAT_GIF, self::FORMAT_JPG, self::FORMAT_PNG, self::FORMAT_WEBP, self::FORMAT_IMAGICK])) {
+        if (! in_array($format, [self::FORMAT_SVG, self::FORMAT_BMP, self::FORMAT_GIF, self::FORMAT_JPG, self::FORMAT_PNG, self::FORMAT_WEBP])) {
             throw QrCodeGeneratorException::invalidFormat();
         }
 
         $options = new QROptions();
+        $options->eccLevel = QRCode::ECC_M;
         $options->outputType = $format;
-        $options->imagickFormat = 'webp';
-        $options->quality = 90;
         $options->scale = 20;
         $options->addQuietzone = true;
         $options->quietzoneSize = 1;
-        $options->outputBase64 = true;
+        $options->imageBase64 = true;
         $this->qrCode = new QRCode($options);
 
         return $this;
